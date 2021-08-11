a13ed4fd82913d746dad66771e9c2394387d3849
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-11 14:20:21 +0200

    Update d_TXT_cmd.ino

--- _News_epaper/d_TXT_cmd.ino
+++ _News_epaper/d_TXT_cmd.ino
@@ -12,6 +12,7 @@ uint16_t getListLength(String list_name){
   while(list_file.available()){
     String line = list_file.readStringUntil('\n');
     line_count++;
+    assert(line_count > 0, "getListLength");
   }
   
   list_file.close();
@@ -22,7 +23,7 @@ uint16_t getListLength(String list_name){
 }
 
 
-String fetchNextBlock(String list_name, uint16_t loop_length, int8_t& type, uint16_t& frame_st, uint16_t& frame_end, uint16_t& repeat, int16_t& wait_period){
+String fetchNextBlock(String list_name, uint16_t loop_length, int8_t& type, uint16_t& frame_st, uint16_t& frame_end, uint16_t& repeat, int16_t& wait_period_sec){
   static uint16_t curr_line = 0;
   char fn_arr[ARRAY_CHARS];
   File list_file;
@@ -35,7 +36,6 @@ String fetchNextBlock(String list_name, uint16_t loop_length, int8_t& type, uint
 
   while(list_file.available()){
     String line = list_file.readStringUntil('\n');
-    //debugPrintln(line);
     
     if(line_count == curr_line){
       
@@ -63,7 +63,7 @@ String fetchNextBlock(String list_name, uint16_t loop_length, int8_t& type, uint
       if(symbol != -1){   // a wait command
         type = WAIT_t;
         String str_wait = line.substring(symbol+1, line.length()-1);   // parse the wait period to the caller
-        wait_period = str_wait.toInt();
+        wait_period_sec = str_wait.toInt();
         break;
       }
 
@@ -79,6 +79,7 @@ String fetchNextBlock(String list_name, uint16_t loop_length, int8_t& type, uint
     }
 
     line_count++;
+    assert(line_count > 0, "fetchNextBlock");
   }
 
   curr_line = (curr_line+1) % loop_length;    // advance playlist
@@ -89,12 +90,7 @@ String fetchNextBlock(String list_name, uint16_t loop_length, int8_t& type, uint
 
 
 bool checkConversionRequest(String file_name){
-  char fn_arr[ARRAY_CHARS];
-  
-  filename = file_name;
-  filename.toCharArray(fn_arr, filename.length() + 1);
-  
-  if(SD.exists(fn_arr)){
+  if(checkFileExists(file_name)){
     debugPrintln("Conversion command found!");
     return true;
   }
@@ -110,20 +106,16 @@ void setConversionDone(String file_name){
   if(SD.exists(fn_arr)){    // check anyway, just in case
     SD.remove(fn_arr);
   }
+  else{
+    assert(ERROR_ASSERT, "tried removing conversion command, but not found.");
+  }
   debugPrintln("Conversion command cleared.");
 }
 
 
 bool checkConversionNeeded(String file_name, uint16_t frame_number_start){
-  char fn_arr[ARRAY_CHARS];
-  
   filename = file_name + frame_number_start + ".h";
-  filename.toCharArray(fn_arr, filename.length() + 1);
-  
-  if(SD.exists(fn_arr)){    // if file already exists, do not perform conversion
-    return false;
-  }
-  return true;
+  return (1 - checkFileExists(filename));
 }
 
 
@@ -157,12 +149,32 @@ String getListLine(String list_name, uint16_t line_number){
       return line.substring(0, line.length()-1);
     }
     line_count++;
+    assert(line_count > 0, "getListLine");
   }
 
   list_file.close();
   return "-1";
 }
 
+
+uint16_t getLastIndex(String list_name){
+  uint16_t llen = getListLength(list_name);
+  String last_fname = getListLine(list_name, llen - 1);
+  String last_ind_str = last_fname.substring(bmp_prefix.length(), last_fname.length()-4);
+  uint16_t last_ind = last_ind_str.toInt();
+  return last_ind;
+}
+
+bool checkFileExists(String file_name){
+  char fn_arr[ARRAY_CHARS];
+  filename = file_name;
+  filename.toCharArray(fn_arr, file_name.length() + 1);
+  
+  if(SD.exists(fn_arr)){
+    return true;
+  }
+  return false;
+}
 
 
 
