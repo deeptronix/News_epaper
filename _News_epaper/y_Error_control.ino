b18491650eeca70c9e4313dfb2e9bf1dead6ac23
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-11 17:30:06 +0200

    Update y_Error_control.ino

--- _News_epaper/y_Error_control.ino
+++ _News_epaper/y_Error_control.ino
@@ -3,10 +3,17 @@
 #define WRITE_RESTART(val) ((*(volatile uint32_t *)RESTART_ADDR) = (val))
 
 
+/*	@brief: writes the registers that cause the Teensy to restart
+*/
 inline void TeensyRestart(){
   WRITE_RESTART(0x5FA0004);
 }
 
+
+/*	@brief: tests a condition and logs an error if it's false.
+	@param: the test to be checked
+	@param: the string to be appended to the error message
+*/
 void assert(bool test, String error_source){
   if(test == false){
     debugPrint("\n * * * ERROR FOUND AT ");
@@ -14,6 +21,11 @@ void assert(bool test, String error_source){
   }
 }
 
+
+/*	@brief: tests a condition; if it's false, it logs an error and reboots the board (the error was critical).
+	@param: the test to be checked
+	@param: the string to be appended to the error message
+*/
 void assertCritical(bool test, String error_source){
   if(test == false){
     debugPrint("\n * * * CRITICAL ERROR FOUND AT ");
@@ -23,3 +35,6 @@ void assertCritical(bool test, String error_source){
     TeensyRestart();
   }
 }
+
+
+// eof
