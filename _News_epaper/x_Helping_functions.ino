e73ce8cca4c8cd465b39074953676b6165b6ef4f
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-11 17:30:19 +0200

    Update x_Helping_functions.ino

--- _News_epaper/x_Helping_functions.ino
+++ _News_epaper/x_Helping_functions.ino
@@ -1,44 +1,68 @@
 
+/*	@brief: performs EPD deghosting; as many cycles, as indicated by its argument. A small delay is
+			introduced in order to allow the particles to completely settle to their state.
+	@param: the number of cycles to perform (note: only in range 0 - 255!)
+*/
 void deghost(uint8_t cycles){
-  epd.ClearFrame();
+  epd.ClearFrame();		// clear the internal EPD image buffer
   for(uint8_t k = 0; k < cycles; k++){
-    epd.DisplayFrame();
+    epd.DisplayFrame();		// updating the display deghosts it
     debugPrintln("Display deghosted (iter. " + String(k + 1) + " out of " + String(cycles) + ").");
     delay(300);
-    epd.WaitUntilIdle();
+    epd.WaitUntilIdle();	// repeat only after the EPD is back online
   }
 }
 
 
-void configureComm(){
+/*	@brief: configures the communication with the ESP32, which is an 8n1 RS232. Also, 2 additional
+			pins are used to receive a request from the ESP32 (IRQ_receive), and give back an
+			acknwoledge (TRIGGER_OUT).
+*/
+inline void configureComm(){
   channel.begin(BAUD);
   pinMode(TRIGGER_OUT, OUTPUT);
   pinMode(IRQ_receive, INPUT);
 }
 
-bool pollESP(){
+
+/*	@brief: reads the state of the IRQ_receive pin, to see if the ESP32 is communicating something.
+	@returns: the state of the pin, active high.
+*/
+inline bool pollESP(){
   return digitalRead(IRQ_receive);
 }
 
-void ackESP(){      // give the ESP32 an acknowledge
+
+/*	@brief: after an incoming request has been asserted, give back the ESP an acknowledge by raising
+			the TRIGGER_OUT pin, until the ESP clears the request.
+			If this takes too much time, the ESP32 must be in a fault condition.
+*/
+void ackESP(){
   digitalWrite(TRIGGER_OUT, 1);
   uint16_t cnt = 0;
   while(digitalRead(IRQ_receive)){
     delay(1);
     cnt++;
-    assert(cnt > 0, "ackESP");
+    assert(cnt > 0, "ackESP");		// this condition will be violated after 65535ms, i.e. on an OVF
   }
   digitalWrite(TRIGGER_OUT, 0);
 }
 
-int32_t pullBuffer(uint8_t* buf, uint32_t len){
-  int32_t i = 0;
+
+/*	@brief: after an ESP32 request for image sending, pull the data through the serial connection
+			until no more data come through.
+	@param: the pointer to the image buffer to be filled with the incoming data
+	@returns: the number of bytes received (for a 300x400 image, we expect 15000 bytes since
+			the image itself is already dithered and byte-aligned).
+*/
+int32_t pullBuffer(uint8_t* buf){
+  int32_t len = 0;
   bool repeat = false;
   
   do{
     while(channel.available() > 0){
-      buf[i++] = channel.read();
-      assert(i > 0, "pullBuffer");
+      buf[len++] = channel.read();
+      assert(len > 0, "pullBuffer");
     }
     delay(10);   // if channel is emptied, wait a little to see if new data comes through
     if(channel.available() > 0){
@@ -51,9 +75,15 @@ int32_t pullBuffer(uint8_t* buf, uint32_t len){
 
   channel.flush();
 
-  return i;
+  return len;
 }
 
+
+/*	@brief: used to crop an incoming message, to see if it matches any of the ones known.
+	@param: the pointer to the incoming data, to be cropped
+	@param: the length of a standard message, fixed in z_Setup.h
+	@returns: the message cropped to its first "len" characters
+*/
 String getFirstChars(uint8_t* data, uint8_t len){
   String msg = "";
   for(uint8_t p = 0; p < len; p++){
@@ -64,7 +94,10 @@ String getFirstChars(uint8_t* data, uint8_t len){
 }
 
 
-
+/*	@brief: substitutes the std serial.print() in order to decide whether to print to serial or 
+			logged to a .txt debug file on the SD card.
+	@param: the text to be printed
+*/
 inline void debugPrint(String text){
   #if DEBUG
     #if LOGGING_TO_SD
@@ -82,6 +115,11 @@ inline void debugPrint(String text){
   #endif
 }
 
+
+/*	@brief: substitutes the std serial.println() in order to decide whether to print to serial or 
+			logged to a .txt debug file on the SD card.
+	@param: the text to be printed
+*/
 inline void debugPrintln(String text){
   #if DEBUG
     #if LOGGING_TO_SD
@@ -99,6 +137,9 @@ inline void debugPrintln(String text){
   #endif
 }
 
+
+/*	@brief: substitutes the std serial.println() when just a CR char is needed (overloading function).
+*/
 inline void debugPrintln(){
   #if DEBUG
   debugPrintln("");
