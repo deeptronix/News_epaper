7dcf59dc69b4a6d3bfcdd2036ec5ee6de82d71f1
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-11 14:22:12 +0200

    Update z_Setup.h

--- _News_epaper/z_Setup.h
+++ _News_epaper/z_Setup.h
@@ -2,102 +2,57 @@
 #define _max(val_a, val_b)  ((val_a > val_b)?  val_a : val_b)
 #define _min(val_a, val_b)  ((val_a < val_b)?  val_a : val_b)
 
+#if LOGGING_TO_SD
 #define debug_filename "debug.txt"
-
-#if FINALIZE_DELIVERY   // The parameters used on THE FINAL PRODUCT
-  // EPD paramters:
-  const uint16_t ep_width = 400, ep_height = 300;
-  #define EP_SIZE (ep_width*ep_height)
-  const uint8_t STARTUP_CLEAR_CY = 2; // needed to clear the previous image, which might have forced pixels
-  #define NIGHT_DEGHOST_CYCLES 1      // cycles per iteration during the night
-  #define MIN_DEGHOST_OFFSET 2
-  #define ABS_MAX_DEGH_CYCLES 5
-  #define AFTER_PHOTO_DEGHOST_CYCLES 1
-  #define DEGHOST_FORMULA  _min(((_max(repeat, MIN_DEGHOST_OFFSET) * total_frames) / 12), ABS_MAX_DEGH_CYCLES)
-  
-  // Communication parameters & pin definitions
-  #define BAUD 57600   // Pins: TX=1,  RX=0
-  #define TRIGGER_OUT 29
-  #define IRQ_receive 30
-  #define channel Serial1
-  #define MSG_LENGTH 5
-  #define MOTION_MSG "MOVMT"
-  #define LOWLIGHT_MSG "NIGHT"
-  #define GOODLIGHT_MSG "GDDAY"
-  
-  
-  // Execution flow parameters:
-  const String playlist = "playlist.txt";    // .txt file that lists every image, animation and pause to be executed sequentially (like a playlist)
-  const String RECONVERT = "convert.txt";    // if this file is found on the SD card, it will perform the dithering+byte alignment and save each header file of all animations; it will then delete this file
-  #define ANIMAT_char '$'
-  #define WAIT_char ':'
-  #define STOP_char '.'
-  #define SLOWDOWN_PLAYBACK_ms 0
-  #define DEFAULT_SLEEP_TIMEOUT_sec 30    // this will be overwritten by the playlist "wait:_" value
-  #define PHOTO_HOLD_PERIOD_sec 900     // 15 minutes pause for the taken photo
-  #define NIGHT_INSTR_PERIOD_sec 2880   // instructions (deghosting) period during the night: 48 minutes, which is 10 times in 8 hours. This can still be interrupted every second by an incoming message
-  
-  
-  // Static images parameters
-  #define HEADER_LENGTH 54
-  #define BUFFPIXEL 80
-  const String photo_list_name = "photos.txt";      // .txt file that lists every available photo taken
-  const String bmp_prefix = "magic";    // prefix used to save new photos taken
-  #define MAGIC_PROBABILITY 60
-  
-  
-  // Appearance parameters:
-  #define ANIMATION_DITHER_TYPE AtkinsonDither
-  #define IMAGES_DITHER_TYPE AtkinsonDither
-
-
-#else   // The parameters used ONLY FOR TESTING
-  // EPD paramters:
-  const uint16_t ep_width = 400, ep_height = 300;
-  #define EP_SIZE (ep_width*ep_height)
-  const uint8_t STARTUP_CLEAR_CY = 2; // needed to clear the previous image, which might have forced pixels
-  #define NIGHT_DEGHOST_CYCLES 1      // cycles per iteration during the night
-  #define MIN_DEGHOST_OFFSET 2
-  #define ABS_MAX_DEGH_CYCLES 5
-  #define AFTER_PHOTO_DEGHOST_CYCLES 1
-  #define DEGHOST_FORMULA  _min(((_max(repeat, MIN_DEGHOST_OFFSET) * total_frames) / 12), ABS_MAX_DEGH_CYCLES)
-  
-  // Communication parameters & pin definitions
-  #define BAUD 38400   // Pins: TX=1,  RX=0
-  #define TRIGGER_OUT 29
-  #define IRQ_receive 30
-  #define channel Serial1
-  #define MSG_LENGTH 5
-  #define MOTION_MSG "MOVMT"
-  #define LOWLIGHT_MSG "NIGHT"
-  #define GOODLIGHT_MSG "GDDAY"
-  
-  
-  // Execution flow parameters:
-  const String playlist = "playlist.txt";    // .txt file that lists every image, animation and pause to be executed sequentially (like a playlist)
-  const String RECONVERT = "convert.txt";    // if this file is found on the SD card, it will perform the dithering+byte alignment and save each header file of all animations; it will then delete this file
-  #define ANIMAT_char '$'
-  #define WAIT_char ':'
-  #define STOP_char '.'
-  #define SLOWDOWN_PLAYBACK_ms 0
-  #define DEFAULT_SLEEP_TIMEOUT_sec 30    // this will be overwritten by the playlist "wait:_" value
-  #define PHOTO_HOLD_PERIOD_sec 30      // pause for the taken photo
-  #define NIGHT_INSTR_PERIOD_sec 2880   // instructions (deghosting) period during the night: 48 minutes, which is 10 times in 8 hours. This can still be interrupted every second by an incoming message
-  
-  
-  // Static images parameters
-  #define HEADER_LENGTH 54
-  #define BUFFPIXEL 80
-  const String photo_list_name = "photos.txt";      // .txt file that lists every available photo taken
-  const String bmp_prefix = "magic";    // prefix used to save new photos taken
-  #define MAGIC_PROBABILITY 20
-  
-  
-  // Appearance parameters:
-  #define ANIMATION_DITHER_TYPE AtkinsonDither  //PersonalFilterDither
-  #define IMAGES_DITHER_TYPE AtkinsonDither
-
 #endif
+
+// EPD paramters:
+const uint16_t ep_width = 400, ep_height = 300;
+#define EP_SIZE (ep_width*ep_height)
+#define STARTUP_CLEAR_CY 2             // needed to clear the image at startup, not knowing where it comes from
+#define NIGHT_DEGHOST_CYCLES 1         // cycles per iteration during the night
+#define AFTER_PHOTO_DEGHOST_CYCLES 1
+#define MIN_DEGHOST_OFFSET 2           // used in the formula below
+#define ABS_MAX_DEGH_CYCLES 5          // used in the formula below
+#define DEGHOST_FORMULA(rep, fn_end, fn_st)  _min(((_max(rep, MIN_DEGHOST_OFFSET) * (fn_end - fn_st + 1)) / 12), ABS_MAX_DEGH_CYCLES)
+
+// Communication parameters & pin definitions
+#define BAUD 57600
+#define TRIGGER_OUT 5
+#define IRQ_receive 6
+#define channel Serial1   // Pins: TX=1 (not used),  RX=0
+#define MSG_LENGTH 5
+#define MOTION_MSG "MOVMT"
+#define LOWLIGHT_MSG "NIGHT"
+#define GOODLIGHT_MSG "GDDAY"
+
+
+// Execution flow parameters:
+const String playlist = "playlist.txt";    // .txt file that lists every image, animation and pause to be executed sequentially (like a playlist)
+const String RECONVERT = "convert.txt";    // if this file is found on the SD card, it will perform the dithering+byte alignment and save each header file 
+                                           // of the animations which are missing the first frame (used in the playlist); it will then delete this file.
+#define ANIMAT_char '$'
+#define WAIT_char ':'
+#define STOP_char '.'
+#define SLOWDOWN_PLAYBACK_ms 0
+#define DEFAULT_SLEEP_TIMEOUT_sec 30    // this will be overwritten by the playlist "wait:_" value
+#define PHOTO_HOLD_PERIOD_sec 30      // 1800       // hold photo taken (inhibit playback) for a couple of minutes
+#define NIGHT_INSTR_PERIOD_sec 4800   // instructions (deghosting) period during the night: 80 minutes, which is 6 times in 8 hours. This can still 
+                                      // be interrupted every 1 sec by an incoming message
+
+
+// Static images parameters
+#define HEADER_LENGTH 54
+#define BUFFPIXEL 80
+const String photo_list_name = "photos.txt";    // .txt file that lists every available photo taken
+const String bmp_prefix = "mag";  	            // prefix used to save new photos taken; the total number of chars must be less than 8, including the suffix number!
+#define MAGIC_PROBABILITY 20    // 60
+
+
+// Appearance parameters:
+#define ANIMATION_DITHER_TYPE AtkinsonDither
+#define IMAGES_DITHER_TYPE AtkinsonDither
+
 
 
 // FSM constants for playlist execution; actual values are not relevant, as long as they are >= 0.
@@ -111,8 +66,12 @@
 #define UNDEF_t 99
 
 
-// Other uncathegorized constants
-#define ARRAY_CHARS 20    // SD supports a maximum of 8+3 characters for filename+.+extension
+// Other uncathegorized constants and Macros
+#define ARRAY_CHARS 20    // SD supports a maximum of 8.3 characters for filename.extension
+#define ERROR_ASSERT 0
+  // Wake ID not used, but available if needed:
+// #define TIMER_WAKE_SOURCE 36
+// #define RTC_ALARM_SOURCE 35
 
 
 
