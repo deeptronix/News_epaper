4f2d6862a3ef309fc14ef79b3cfd0ba8d29270ad
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-11 14:19:54 +0200

    Update c_Header_load.ino

--- _News_epaper/c_Header_load.ino
+++ _News_epaper/c_Header_load.ino
@@ -1,45 +1,4 @@
 
-uint8_t header_400x300[HEADER_LENGTH] = {     // extracted from an actual 400x300 bitmap file; not compatible with other resolutions!
-  0x42, 0x4D, 0x76, 0x7E, 0x05, 0x00, 0x00, 0x00, 0x00,
-  0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00, 0x00, 0x00,
-  0x90, 0x01, 0x00, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x01,
-  0x00, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0xC4, 0x0E, 0x00, 0x00, 0xC4, 0x0E, 0x00,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-};
-
-String savePhotoBMP(uint8_t* img, String bmp_prefix, String list_name, uint16_t last_index){
-
-  uint8_t out_values[8];
-  uint8_t r, g, b;
-  uint8_t pixel;
-  char fn_arr[ARRAY_CHARS];
-  File bmp_file;
-  
-  filename = bmp_prefix + String(last_index + 1) + ".bmp";
-  filename.toCharArray(fn_arr, filename.length() + 1);
-  bmp_file = SD.open(fn_arr, FILE_WRITE);
-
-  for(uint8_t i = 0; i < HEADER_LENGTH; i++){
-    bmp_file.write(header_400x300[i]);
-  }
-  for(uint32_t p = 0; p < ((ep_width/8)*ep_height); p++){
-    pixel = img[p];
-    img_conv.bool8bitsTo8Bytes(pixel, out_values);
-    for(uint8_t e = 0; e < 8; e++){
-      convert.colorBoolTo888(out_values[e], r, g, b);
-      bmp_file.write(r);
-      bmp_file.write(g);
-      bmp_file.write(b);
-    }
-  }
-
-  bmp_file.close();
-
-  return (filename.toUpperCase());    // limitation of native SD library; the saved filename will always be uppercase; hence, return an appropriately converted filename.
-}
-
-
 void animationCompress(String curr_filename, uint16_t frame_number_start, uint16_t total_frames){
   char fn_arr[ARRAY_CHARS];
 
@@ -85,6 +44,7 @@ void fileLoad(char* fn_arr, uint8_t* out_buffer, uint32_t size){
   for(uint32_t i = 0; i < size; i++){
     out_buffer[i] = compressed_file.read();
   }
+  compressed_file.close();
 }
 
 
