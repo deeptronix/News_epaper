a40885906c289a1552a313e55ebeb263197e048f
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-11 16:45:03 +0200

    Update e_Sleep_setup.ino

--- _News_epaper/e_Sleep_setup.ino
+++ _News_epaper/e_Sleep_setup.ino
@@ -1,36 +1,32 @@
 
-/*
- // Requires definition of object: SnoozeBlock config_teensy35(alarm);
- // and of object: SnoozeAlarm  alarm;
-inline void setSleepTimer_sec(int32_t tot_sec){
-  if(tot_sec < 0){
-    assert(ERROR_ASSERT, "tried loading a negative value in RTC timer.");
-    return;
-  }
-  uint8_t hours = tot_sec / 3600;
-  uint8_t mins = (tot_sec / 60) % 60;
-  uint8_t secs = tot_sec % 60;
-  alarm.setRtcTimer(hours, mins, secs);   // hour, min, sec
-}
+/*	@brief: Sets the sleep timer to 1000 milliseconds, fixed in SW because other timing hypothesis are based on this behaviour
 */
-
 inline void initSleep(){
   setSleepTimer_ms(1000);    // set sleep timer to 1 sec
 }
 
+
+/*	@brief: loads the timer value in the sleep timer function
+	@param: the amount of ms to sleep for, once the sleep function is called.
+*/
 inline void setSleepTimer_ms(int32_t tot_ms){
   if(tot_ms < 0){
     assert(ERROR_ASSERT, "tried loading a negative value in Low Power timer.");
-    return;
   }
-  
-  #if defined(__IMXRT1062__)    // Teensy 4.0
-  timer.setTimer(tot_ms / 1000);// seconds
-  #else
-    timer.setTimer(tot_ms);// milliseconds
-  #endif
+  else{
+	  // depending on the underlying architecture, sets the correct timer value
+	#if defined(__IMXRT1062__)    	// Teensy 4.0
+	timer.setTimer(tot_ms / 1000);	// seconds
+	#else
+	timer.setTimer(tot_ms);		// milliseconds
+	#endif
+  }
 }
 
+
+/*	@brief: Puts the processor to sleep (low power mode); the time is set by loading the
+			timer with the appropriate duration beforehand (using the previous functions).
+*/
 inline void sleepTeensy(){
   Snooze.sleep(config_teensy35);
 }
