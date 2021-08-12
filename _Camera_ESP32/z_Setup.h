538cf37327b5d221137a0558dc710eda5d2ece34
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-12 15:47:29 +0200

    Update z_Setup.h

--- _Camera_ESP32/z_Setup.h
+++ _Camera_ESP32/z_Setup.h
@@ -34,26 +34,26 @@ const uint16_t ep_width = 400, ep_height = 300;
 
 
 // Flow parameters
-#define STARTUP_DELAY_sec 2 // 15    // give Teensy some time to deghost and start the playlist
+#define STARTUP_DELAY_sec 15    // give Teensy some time to deghost and start the playlist
 #define MIN_IDLE_DELAY_ms 20
 #define TEENSY_COMM_TIMEOUT_ms 120000   //  after this many ms the communication with Teensy is known to be broken, so we restart the ESP32
 
 #define FACE_SEARCH_INTERVAL_SLOW_ms 4000   // when no motion is detected (and after FACE_SEARCH_STOP_ms), search for faces but not frequently
 #define FACE_SEARCH_INTERVAL_FAST_ms 1000    // once motion is detected, start searching for faces quickly
 #define FACE_SEARCH_STOP_ms 40000        // after this many ms, the face detection will again slow down in order to reduce false triggering
-#define PHOTO_HOLD_PERIOD_sec 30    // 1800    // hold photo taken (inhibit new acquisition) for some time
+#define PHOTO_HOLD_PERIOD_sec 1800    // hold photo taken (inhibit new acquisition) for some time
 #define FACES_PER_SESSION 1     // can be as low as 0; 1 face will always be needed in order to start detection loop
 #define SESSION_FRAMES 15       // once a face is detected, search for this many times if a number of FACES_PER_SESSION faces is detected.
 
 #define PIXEL_LIGHT_THRESHOLD 80    // if pixels have a lightness higher than this value (range is 0~255), it's considered sufficiently exposed to light
 #define MIN_LIGHT_PERCENT 30    // if at least this percentage of pixels is above the aforementioned threshold, consider the room to be lit
-#define LOWLIGHT_FRAME_CNT 2  // 3      // count this many frames before telling Teensy to go into "night" mode
+#define LOWLIGHT_FRAME_CNT 3      // count this many frames before telling Teensy to go into "night" mode
 #define LIGHT_SEARCH_INTERVAL_sec 20   // how often to check the state of room illumination
 
 #define MOTION_SEARCH_INTERVAL_ms 200
 #define PIXEL_MOTION_THRESHOLD 20     // if a pixel sees a difference of at least this much, with respect to the comparison image buffer, count it for motion detection. range is 0~255
 #define MOTION_DETECT_PERCENT 20      // once above this percentage threshold, consider enough motion has happened in the frame
-#define AFTERMOTION_INHIBIT_sec 60  // 3600     // once motion is triggered, how long to wait before telling Teensy to resume playback again (basically, an inhibition delay on motion detection)
+#define AFTERMOTION_INHIBIT_sec 3600     // once motion is triggered, how long to wait before telling Teensy to resume playback again (basically, an inhibition delay on motion detection)
 
   // The messages that can be sent to Teensy to communicate states to go in. Only exactly MSG_LENGTH characters messages are allowed!
 #define MSG_LENGTH 5
