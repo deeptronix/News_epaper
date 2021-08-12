416c0f10feb71adebc504e57245603d9f2392f98
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-08-12 13:15:53 +0200

    Update x_Error_control.ino

--- _Camera_ESP32/x_Error_control.ino
+++ _Camera_ESP32/x_Error_control.ino
@@ -1,8 +1,20 @@
 
+/*	@brief: tests a condition and logs an error if it's false. By default, tells Teensy via Serial
+			the error codename.
+	@param: the test to be checked
+	@param: the string to be appended to the error message
+*/
 void _assert(bool test, String error_source){
   return _assert(test, error_source, true);
 }
 
+
+/*	@brief: tests a condition and logs an error if it's false. By default, tells Teensy via Serial
+			the error codename.
+	@param: the test to be checked
+	@param: the string to be appended to the error message
+	@param: whether or not to communicate to Teensy via serial the error codename.
+*/
 void _assert(bool test, String error_source, bool tell_teensy){
   if(test == false){
     debugPrint("\n * * * ERROR FOUND AT ");
@@ -15,10 +27,21 @@ void _assert(bool test, String error_source, bool tell_teensy){
   }
 }
 
+
+/*	@brief: tests a condition; if it's false, it logs an error and reboots the board (the error was critical).
+	@param: the test to be checked
+	@param: the string to be appended to the error message
+*/
 void _assertCritical(bool test, String error_source){
   _assertCritical(test, error_source, true);
 }
 
+
+/*	@brief: tests a condition; if it's false, it logs an error and reboots the board (the error was critical).
+	@param: the test to be checked
+	@param: the string to be appended to the error message
+	@param: whether or not to communicate to Teensy via serial the error codename.
+*/
 void _assertCritical(bool test, String error_source, bool tell_teensy){
   if(test == false){
     debugPrint("\n * * * CRITICAL ERROR FOUND AT ");
