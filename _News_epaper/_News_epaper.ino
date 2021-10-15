4efeec8e200e79e51e6355ba77947905a33db623
deeptronix <36998673+deeptronix@users.noreply.github.com>
2021-10-15 22:30:33 +0200

    Update _News_epaper.ino

--- _News_epaper/_News_epaper.ino
+++ _News_epaper/_News_epaper.ino
@@ -1,3 +1,4 @@
+// © 2021 by Deep Tronix. Visit my website @ rebrand.ly/deeptronix
 // This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 2.0 Generic License.
 // To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/2.0/ or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
